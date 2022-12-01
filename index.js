52e81486b2edc2ea71a2c26af7707d2de4e22f44
Talha Shaukat <76585876+Talha-dev1980@users.noreply.github.com>
2022-12-01 18:18:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+app.all('/:name', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Hey '+req.params.name)
+})
 app.listen(process.env.PORT || 3000)
